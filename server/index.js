f2cfcaa575f0ce29d9526c93b2a8a4d737c6e95b
Plinio Prado <plinio.prado@immaginare.com.br>
2017-09-08 14:27:23 -0700

    Finished feature.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-import admin from './database/firebase';
+// Dependencies
+const express = require('express')
+
+// Dependencies - routes
+const rest = require('./apirest/rest')
+
+// App
+const app = express()
+
+// Routes
+app.get('/', function (req, res) { res.status(400).send('invalid') })
+app.use('/rest', rest);
+ 
+// Run
+app.listen(4400, function () {
+    console.log('Server running on port 4400')
+})
