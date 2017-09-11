4bcf695415788cb9ca6e9c95dc5a6ffbd72e7caa
Kat Solberg <katsolberg@gmail.com>
2017-09-11 12:09:51 -0700

    add single ingredient and event scenes, link to navigation

--- js/scenes/HealthBenefit/HealthBenefit.js
+++ js/scenes/HealthBenefit/HealthBenefit.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { withNavigation } from 'react-navigation';
 
 import { styles } from './styles'
 
-const HealthBenefits = () => (
+const HealthBenefits = ({ navigation }) => (
     <View style={styles.container}>
         <Text>This is HealthBenefits scene</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Ingredient')}>
+            <Text> Take me to an ingredient </Text>
+        </TouchableOpacity>
     </View>
 )
 
-HealthBenefits.propTypes = {}
+HealthBenefits.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func,
+    }).isRequired
+}
 
-export default HealthBenefits
+const HealthBenefitsWithNavigation = withNavigation(HealthBenefits)
+
+export default HealthBenefitsWithNavigation;
