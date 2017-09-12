769183ec95ef4f6a033cf127c9479e0457ce0885
Kat Solberg <katsolberg@gmail.com>
2017-09-12 10:08:19 -0700

    cleanup extra files from merge conflict

--- js/components/Featured/styles.js
+++ js/components/Featured/styles.js
@@ -1,35 +1,9 @@
-import { StyleSheet } from 'react-native';
-
-import { colors } from '../../config/styles';
+import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'white',
     },
-    list: {
-        flex: 1,
-        borderTopWidth: 0,
-    },
-    listItem: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 280,
-        borderBottomWidth: 1,
-        borderTopWidth: 1,
-        borderRightWidth: 1,
-        borderLeftWidth: 1,
-        borderBottomColor: colors.grey,
-        borderColor: colors.grey,
-        margin: 5,
-        borderRadius: 5,
-        height: 42,
-
-    },
-    title: {
-        paddingLeft: 15,
-        fontSize: 16,
-    }
 })
