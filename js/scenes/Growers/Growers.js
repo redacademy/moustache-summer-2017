b4f86eba0b2058bc8ff43653f849eb8e8504c751
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-09-13 16:00:36 -0700

    finished growers list

--- js/scenes/Growers/Growers.js
+++ js/scenes/Growers/Growers.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, Image, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
+import LineSeparator from '../../components/LineSeparator/';
+import { colors } from '../../config/styles';
 
-const Growers = ({ growersList }) => (
-    <View style={styles.container}>
-        <Text>This is Growers scene</Text>
-    </View>
-);
+const Growers = ({ growersList, growerLink }) => (
+    <ScrollView>
+        <View style={styles.container}>
+        {
+            growersList.map((grower) => (
+                <View key={grower.id} style={styles.mainContainer}>
+                    <View style={styles.growerContainer}>
+                        <TouchableOpacity onPress={() => growerLink(grower.websiteLink)}>
+                            <Image source={{uri: grower.logoLink}} style={styles.logo}/>
+                        </TouchableOpacity>
+                        <Text style={styles.name}>{grower.name}</Text>
+                        <Text style={styles.description}>{grower.description}</Text>
+                    </View>
+                    <LineSeparator 
+                        additionalStyles={{height: 1, width: '100%', backgroundColor: colors.lightGreen}} 
+                    />
+                </View>
+            ))
+        }
+        </View>
+    </ScrollView>
+)
 
 Growers.propTypes = {
     growersList: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.string,
             name: PropTypes.string,
             description: PropTypes.string,
             logoLink: PropTypes.string,
             websiteLink: PropTypes.string
         })
-    )
+    ),
+    growerLink: PropTypes.func.isRequired
 }
 
 export default Growers
--- js/scenes/Growers/styles.js
+++ js/scenes/Growers/styles.js
@@ -1,9 +1,44 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native';
+import { colors, typography } from '../../config/styles';
 
 export const styles = StyleSheet.create({
     container: {
+        width: '100%',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: colors.white
     },
+    mainContainer: {
+        width: '100%'
+    },
+    growerContainer: {
+        padding: 10
+    },
+    logo: {
+        height: 150,
+        width: 'auto',
+        resizeMode: 'contain'
+    },
+    name: {
+        paddingBottom: 5,
+        paddingLeft: 5,
+        paddingRight: 5,
+        color: colors.grey,
+        fontFamily: typography.fontBook,
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    description: {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 5,
+        color: colors.grey,
+        fontFamily: typography.fontBook,
+        fontSize: 15,
+    },
+    line: {
+        width: '100%',
+        color: colors.lightGreen
+    }
 })
