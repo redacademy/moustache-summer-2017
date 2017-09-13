6d70c7d2cf279becce6e0367eaf5c7cdb4828dc4
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-09-13 11:30:38 -0700

    remove commented code

--- js/config/apolloClient.js
+++ js/config/apolloClient.js
@@ -8,12 +8,4 @@ const client = new ApolloClient({
   networkInterface: networkInterface
 });
 
-// networkInterface.use([{
-//   applyBatchMiddleware(req, next) {
-//       if (!req.options.headers) {
-//           req.options.headers = {}; // Create header object if needed
-//       }
-//     }
-// }]);
-
 export default client;
