717176cd79ae337f61363f824d5d221e03c37a26
Plinio Prado <plinio.prado@immaginare.com.br>
2017-09-20 14:51:28 -0700

    favourites coded to dynamic

--- js/scenes/Favourite/FavouriteContainer.js
+++ js/scenes/Favourite/FavouriteContainer.js
@@ -3,31 +3,48 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { ActivityIndicator } from 'react-native';
 import { addNavigationHelpers } from 'react-navigation';
-
 import Favourite from './Favourite';
 import { storeMenuItem } from '../../redux/modules/menuItems';
 import { colors } from '../../config/styles';
 
+import { triggerStoreMenuItem } from '../../redux/modules/menuItems';
+import { ActivityIndicator } from 'react-native';
+import { favesQuery } from '../../redux/modules/user';
+
 class FavouriteContainer extends Component {
+
     static navigationOptions = {
         title: 'Favourites',
         headerTintColor: 'white',
         headerStyle: { backgroundColor: colors.lightGreen },
     };
 
+    componentWillMount() {
+        const { favesQuery } = this.props
+        favesQuery()
+    }
+
+    addFaves = (list) => {
+        if (!list) return []
+        const faveList = this.props.faves;
+        const newList = list.map(item => ({ ...item, fave: !!faveList.find(it => it.id == item.id)}));
+        return newList;
+    }
+
     sendMenuItem = (item) => {
-        return this.props.dispatch(storeMenuItem(item));
+        const { triggerStoreMenuItem } = this.props;
+        triggerStoreMenuItem(item);
     }
 
     render() {
         const { data: { loading, menuItems }, navigationState, dispatch } = this.props;
-
+        const itemsWithFaves = this.addFaves(menuItems).filter(item => item.fave);
+        
         if (loading) return <ActivityIndicator />;
         return (
             <Favourite
-                favouriteItems={menuItems}
+                favouriteItems={itemsWithFaves}
                 sendMenuItem={this.sendMenuItem}
                 navigation={
                     addNavigationHelpers({
@@ -57,8 +74,8 @@ const fetchMenuItems = gql`
 
 function mapStateToProps(state) {
     return {
-        navigationState: state.home
-        //TO ADD: faves: state.faves.faves
+        navigationState: state.home,
+        faves: state.user.faves
     }
 }
 
@@ -77,8 +94,16 @@ FavouriteContainer.propTypes = {
             }).isRequired
         ),
     }),
-    dispatch: PropTypes.func.isRequired
+    faves: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string
+        })
+    ),
+    dispatch: PropTypes.func,
+    favesQuery: PropTypes.func.isRequired,
+    navigationState: PropTypes.func,
+    triggerStoreMenuItem: PropTypes.func,
 }
 
 const FavouriteItemsWithData = graphql(fetchMenuItems)(FavouriteContainer);
-export default connect(mapStateToProps)(FavouriteItemsWithData);
+export default connect(mapStateToProps, { favesQuery, triggerStoreMenuItem } )(FavouriteItemsWithData);
