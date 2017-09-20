ad22969aacfec47b1ffb8c81323abc44359fd6c6
Kat Solberg <katsolberg@gmail.com>
2017-09-20 14:51:51 -0700

    cleanup growers scene

--- js/scenes/Growers/GrowersContainer.js
+++ js/scenes/Growers/GrowersContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { ActivityIndicator, Linking } from 'react-native';
